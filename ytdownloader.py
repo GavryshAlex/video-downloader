6f1b72c910a7dc6a658706454be82e4bcfb9e333
GavryshAlex <alexgavryshua@gmail.com>
2021-10-22 21:53:32 +0300

    Add GUI

--- ytdownloader.py
+++ ytdownloader.py
@@ -1,12 +1,34 @@
+from tkinter import *
+from tkinter import messagebox
 from pytube import YouTube
-url = str(input())
-path = str(input())
+url = ""
+path = ""
+window = Tk()
+window.title("YTD")
+window.geometry("200x125")
+window.resizable(0,0)
+labelUrl = Label(text="Link")
+entryUrl = Entry()
+labelPath = Label(text="Path")
+entryPath = Entry()
 def download(url, path):
-    try:
-        yt = YouTube(url)
-        yt.streams.first().download(path)
-        return "Video in downloaded"
-    except Exception:
-        return "Error occurred"
+    yt = YouTube(url)
+    yt.streams.first().download(path)
+    messagebox.showinfo("Video is downloaded")
+        #return "Video in downloaded"
+    #except Exception:
+     #   messagebox.showinfo("Error occurred")
+        #return "Error occurred"
+button = Button(text="Download", command = lambda: download(url, path))
+labelUrl.pack()
+entryUrl.pack()
+labelPath.pack()
+entryPath.pack()
+button.pack()
+while(True):
+    url = entryUrl.get()
+    path = entryPath.get()
+    window.update()
 
-print(download(url, path))
+
+window.mainloop()
